163514618e077a8e66051017d3c12f363aafd13a
akg <akg.bit@gmail.com>
2020-07-08 15:48:16 +0530

    Fourth Test case - Add unknown numbers - added

--- src/test/java/com/anandgautam/tdd/StringCalculatorTest.java
+++ src/test/java/com/anandgautam/tdd/StringCalculatorTest.java
@@ -17,7 +17,13 @@ public class StringCalculatorTest extends TestCase {
 
     /* Test two numbers */
     public void testAddTwoNumber() {
-        assertEquals(5, new StringCalculator().Add("2,3"));
+        assertEquals(2+3, new StringCalculator().Add("2,3"));
+    }
+
+    /* Test multiple numbers */
+    public void testAddMultpleNumbers() {
+        assertEquals(1+2+3+4+5+6+7+8+9, new StringCalculator().Add("1,2,3,4,5,6,7,8,9"));
+        assertEquals(1+1+1+1+1+1+1+1+1, new StringCalculator().Add("1,1,1,1,1,1,1,1,1"));
     }
 
 }
