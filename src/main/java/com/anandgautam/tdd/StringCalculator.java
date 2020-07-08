8e81b80435de730887931f9ae61233f68dc9814e
akg <akg.bit@gmail.com>
2020-07-08 18:01:53 +0530

    Seventh Test case - negative numbers  - success criteria added

--- src/main/java/com/anandgautam/tdd/StringCalculator.java
+++ src/main/java/com/anandgautam/tdd/StringCalculator.java
@@ -1,8 +1,11 @@
 package com.anandgautam.tdd;
 
+import java.util.LinkedList;
+import java.util.List;
+
 public class StringCalculator {
 
-    public int Add(String numbers) {
+    public int Add(String numbers) throws NegativeNumberException {
 
         int sum = 0;
         if (numbers.isEmpty()) {
@@ -15,23 +18,46 @@ public class StringCalculator {
             numbers = numbers.substring(4, numbers.length());
         }
 
+        List<String> negativeNumbers = new LinkedList<>();
+
         if ((numbers.contains(delim) && numbers.split(delim).length > 0) || (numbers.contains("\n") && numbers.split("\n").length > 0)) {
             for (String n : numbers.split(delim)) {
                 if (n.contains("\n")) {
                     for (String finalNumber : n.split("\n")) {
                         if (!finalNumber.isEmpty()) {
-                            sum += Integer.parseInt(finalNumber);
+                            if(Integer.parseInt(finalNumber) < 0) {
+                                negativeNumbers.add(finalNumber);
+                            }
+                            if(negativeNumbers.size() == 0) {
+                                sum += Integer.parseInt(finalNumber);
+                            }
                         }
                     }
                 }
                 else {
-                    sum+= Integer.parseInt(n);
+                    if(Integer.parseInt(n) < 0) {
+                        negativeNumbers.add(n);
+                    }
+                    if(negativeNumbers.size() == 0) {
+                        sum+= Integer.parseInt(n);
+                    }
                 }
+            }
+            if(negativeNumbers.size() > 0) {
+                throw new NegativeNumberException("negative numbers are not allowed : " + negativeNumbers.toString());
             }
             return sum;
         }
 
-        sum += Integer.parseInt(numbers);
+        if(Integer.parseInt(numbers) < 0) {
+            negativeNumbers.add(numbers);
+        }
+        if(negativeNumbers.size() == 0) {
+            sum += Integer.parseInt(numbers);
+        }
+        if(negativeNumbers.size() > 0) {
+            throw new NegativeNumberException("negative numbers are not allowed : " + negativeNumbers.toString());
+        }
 
         return sum;
     }
--- src/test/java/com/anandgautam/tdd/StringCalculatorTest.java
+++ src/test/java/com/anandgautam/tdd/StringCalculatorTest.java
@@ -1,41 +1,59 @@
 package com.anandgautam.tdd;
 
+import org.junit.Test;
+
 import junit.framework.TestCase;
 
 public class StringCalculatorTest extends TestCase {
 
     /* Test Blank String input */
-    public void testAddZeroNumber() {
+    public void testAddZeroNumber() throws NegativeNumberException {
         assertEquals(0, new StringCalculator().Add(""));
     }
 
-    /* Test a single string input*/
-    public void testAddOneNumber() {
+    /* Test a single string input */
+    public void testAddOneNumber() throws NegativeNumberException {
         assertEquals(5, new StringCalculator().Add(String.valueOf(5)));
         assertEquals(2, new StringCalculator().Add("2,"));
     }
 
     /* Test two numbers */
-    public void testAddTwoNumber() {
-        assertEquals(2+3, new StringCalculator().Add("2,3"));
+    public void testAddTwoNumber() throws NegativeNumberException {
+        assertEquals(2 + 3, new StringCalculator().Add("2,3"));
     }
 
     /* Test multiple numbers */
-    public void testAddMultpleNumbers() {
-        assertEquals(1+2+3+4+5+6+7+8+9, new StringCalculator().Add("1,2,3,4,5,6,7,8,9"));
-        assertEquals(1+1+1+1+1+1+1+1+1, new StringCalculator().Add("1,1,1,1,1,1,1,1,1"));
+    public void testAddMultpleNumbers() throws NegativeNumberException {
+        assertEquals(1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9, new StringCalculator().Add("1,2,3,4,5,6,7,8,9"));
+        assertEquals(1 + 1 + 1 + 1 + 1 + 1 + 1 + 1 + 1, new StringCalculator().Add("1,1,1,1,1,1,1,1,1"));
     }
 
     /* Test new line between numbers */
-    public void testAddNewLineBetweenNumbers() {
-        assertEquals(1+2+3, new StringCalculator().Add("1\n2,3"));
-        assertEquals(1+2+3, new StringCalculator().Add("1\n2\n3"));
+    public void testAddNewLineBetweenNumbers() throws NegativeNumberException {
+        assertEquals(1 + 2 + 3, new StringCalculator().Add("1\n2,3"));
+        assertEquals(1 + 2 + 3, new StringCalculator().Add("1\n2\n3"));
     }
 
     /* Test different delimiters */
-    public void testAddDifferentDelimiters() {
-        assertEquals(1+2, new StringCalculator().Add("//;\n1;2"));
-        assertEquals(1+2+3, new StringCalculator().Add("//&\n1&2&3"));
+    public void testAddDifferentDelimiters() throws NegativeNumberException {
+        assertEquals(1 + 2, new StringCalculator().Add("//;\n1;2"));
+        assertEquals(1 + 2 + 3, new StringCalculator().Add("//&\n1&2&3"));
+    }
+
+    @Test
+    public void testAddNegativeNumbers() {
+        try {
+            new StringCalculator().Add("//;\n-1");
+        } catch (NegativeNumberException e) {
+            assertEquals("negative numbers are not allowed : [-1]", e.getMessage());
+        }
+
+
+        try {
+            new StringCalculator().Add("//&\n-1&2&-3");
+        } catch (NegativeNumberException e) {
+            assertEquals("negative numbers are not allowed : [-1, -3]", e.getMessage());
+        }
     }
 
 }
