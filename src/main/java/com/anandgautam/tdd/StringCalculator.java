d96f2438a69cf96d411da3d9613aa05ee19cd1ae
akg <akg.bit@gmail.com>
2020-07-08 14:18:55 +0530

    Second test case passing criteria code commited

--- src/main/java/com/anandgautam/tdd/StringCalculator.java
+++ src/main/java/com/anandgautam/tdd/StringCalculator.java
@@ -3,7 +3,14 @@ package com.anandgautam.tdd;
 public class StringCalculator {
     
     public int Add(String numbers) {
+        
+        int sum = 0;
+        if(numbers.isEmpty()) {
+            return 0;
+        }
 
-        return 0;
+        
+        sum+= Integer.parseInt(numbers);
+        return sum;
     }
 }
